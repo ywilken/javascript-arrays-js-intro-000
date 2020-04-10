2901473782f6e05bd4b792f482842280c83dfdc5
Yannick Wilken <yannickwilken99@gmail.com>
2020-04-10 18:08:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,7 @@ function accessElementInArray(array, index) {
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array.shift();
 }
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
