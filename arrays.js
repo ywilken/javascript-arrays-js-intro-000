261c79d9c08216722481e985c15fa9e968a32968
Yannick Wilken <yannickwilken99@gmail.com>
2020-04-10 17:21:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,7 @@ function addElementToBeginningOfArray(array, element) {
 function destructivelyAddElementToBeginningOfArray(array, element) {
   return array.unshift(element);
 }
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
