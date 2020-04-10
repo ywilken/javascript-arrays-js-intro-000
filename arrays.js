087070cb3b3bcf9f979ef799a0020e20ba8ab73d
Yannick Wilken <yannickwilken99@gmail.com>
2020-04-10 18:02:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,3 +19,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift();
+}
